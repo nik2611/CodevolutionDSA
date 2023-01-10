f180e7d0e06eab1f9b780a19b02cfaf3388d3f6d
nikhilgangeri <nikhilgangeri@gmail.com>
2023-01-10 23:57:22 +0530

    wrote time complexity for my factorial of a number program

--- factorial.js
+++ factorial.js
@@ -2,12 +2,12 @@ function factorial(n) {
     let factorial = 1;
     for(let i=2; i <= n; i++) {
 
-        factorial *= i;
+        factorial *= i;  // Time complexity is O(n)
 
     }
     return factorial;
 }
 
-console.log(factorial(4));
-console.log(factorial(5));
 console.log(factorial(0));
+console.log(factorial(1));
+console.log(factorial(5));
