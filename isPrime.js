dffb7541178bcdfd2f24d5260155d5cfb472584a
nikhilgangeri <nikhilgangeri@gmail.com>
2023-01-11 23:59:24 +0530

    added optimized primality approach to isPrime problem

--- isPrime.js
+++ isPrime.js
@@ -1,10 +1,10 @@
 function isPrime(n) {
 
-    if(n == 1){
+    if(n == 1){     // better if used n < 2
         return false;
     }
 
-    for(let i=2; i<n; i++){
+    for(let i=2; i<n; i++){  //Time Complexity is linear O(n) for this approach
 
         if(n % i == 0) {
             return false;
@@ -14,7 +14,26 @@ function isPrime(n) {
 
     return true;
 
-}
+} 
+
+/* Optimized primality approach
+function isPrime(n) {
+
+    if(n == 1){     // better if used n < 2
+        return false;
+    }
+
+    for(let i=2; i<= Math.sqrt(n); i++){  //Time Complexity is O(sqrt(n)) for this approach
+
+        if(n % i == 0) {
+            return false;
+        }
+        
+    }
+
+    return true;
+
+} */
 
 console.log(`is 21 a prime number: ${isPrime(21)}`);
 console.log(`is 7 a prime number: ${isPrime(7)}`);
