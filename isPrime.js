f0c31de5dcb65613c67d908002f263c6ccb9a04b
nikhilgangeri <nikhilgangeri@gmail.com>
2023-01-11 23:44:10 +0530

    made small correction regarding the case of n=1 in isPrime problem

--- isPrime.js
+++ isPrime.js
@@ -1,18 +1,21 @@
 function isPrime(n) {
 
-if (n < 1){
-    console.log("enter natural numbers greater than 1");
-}
-else if(n > 1){
+    if(n == 1){
+        return false;
+    }
 
     for(let i=2; i<n; i++){
+
         if(n % i == 0) {
             return false;
         }
+        
     }
+
     return true;
-}
+
 }
 
 console.log(`is 21 a prime number: ${isPrime(21)}`);
 console.log(`is 7 a prime number: ${isPrime(7)}`);
+console.log(`is 1 a prime number: ${isPrime(1)}`);
