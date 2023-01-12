84a5eb0574ac7d1ecacf32712b4299db75faafc6
nikhilgangeri <nikhilgangeri@gmail.com>
2023-01-13 00:15:05 +0530

    modified output of prime numbers from an array of numbers

--- isPrimeInArray.js
+++ isPrimeInArray.js
@@ -4,16 +4,20 @@ let arr = [1, 8, 3, 4, 5, 6, 7, 2, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 21
 //let arr=[54,89,61,73,99,111,125]
 
 
-function primeNumber(arr) {
+function primeNumber(number) {
     let prime = [];
     let result = false;
-    for (let i=0;i<arr.length;i++) {
-        for (let j = 2; j <= Math.sqrt(arr[i]); j++) {
-            if (arr[i] % j == 0) {
-               delete  arr[i]
+    
+        for (let i = 2; i <= Math.sqrt(number); i++) {
+            if (number % i == 0) {
+               return "is not a prime number"
             }
         }
+        return "is a prime number"
     }
-    return arr
-}
-console.log("Resultant prime numbers array", primeNumber(arr));
+    
+
+
+    for (let i=0;i<arr.length;i++) {
+console.log(`${arr[i]}, ${primeNumber(arr[i])} \n`);
+    }
