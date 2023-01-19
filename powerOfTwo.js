122179440376ce3a8dbbf3462d813aecabf47239
nikhilgangeri <nikhilgangeri@gmail.com>
2023-01-20 00:01:45 +0530

    commented better approach with constant time complexity

--- powerOfTwo.js
+++ powerOfTwo.js
@@ -1,7 +1,7 @@
 function powerOfTwo(n) {
 
     while(n%2 == 0){
-       n /= 2;
+       n /= 2;                // Big(O) is O(logn)
     }
 
     if (n == 1){
@@ -13,5 +13,19 @@ function powerOfTwo(n) {
 
 }
 
-console.log(powerOfTwo(8));
-console.log(powerOfTwo(13));
+console.log(powerOfTwo(-16));
+console.log(powerOfTwo(12));
+
+/* Better approach with O(1) constant time complexity
+
+function powerOfTwo(n) {
+
+    if (n < 1){
+        return false
+    }
+    return (n & (n-1)) === 0;
+
+} 
+*/
+
+
