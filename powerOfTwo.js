6898f0a693eee8b1e8f642d9ddcdd7fe58552d0d
nikhilgangeri <nikhilgangeri@gmail.com>
2023-01-21 22:14:18 +0530

    Was learning NodeJS concepts which are not related to the current problem

--- powerOfTwo.js
+++ powerOfTwo.js
@@ -1,3 +1,4 @@
+
 function powerOfTwo(n) {
 
     if(n < 1){
@@ -19,6 +20,8 @@ function powerOfTwo(n) {
 
 console.log(powerOfTwo(-16));
 console.log(powerOfTwo(12));
+//console.trace("stack trace errors"); //Was learning NodeJS concepts which are not related to the current problem
+console.log(arguments); 
 
 /* Better approach with O(1) constant time complexity
 
