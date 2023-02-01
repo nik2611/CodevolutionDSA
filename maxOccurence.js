2c4853d79168620a49dcc204d9af5dddf57e38b7
nikhilgangeri <nikhilgangeri@gmail.com>
2023-02-01 23:46:57 +0530

    started maxOccurence in an array problem

---
+++ maxOccurence.js
@@ -0,0 +1,34 @@
+function maxOccurence(arr) {
+
+    
+// const maxOccMap = new Map();
+
+// for(let i = 0; i < arr.length; i++){
+
+//     if(maxOccMap.has(arr[i])){
+//         let counter = maxOccMap.get(arr[i]);
+//         counter += 1;
+//         maxOccMap.set(arr[i], counter);
+//     } else{
+//         maxOccMap.set(arr[i], 1);
+//         }    
+//     }
+//  console.log(Math.max(maxOccMap.entries()));
+
+let counterArr = [{1:1}];
+console.log(counterArr[1]);
+
+
+// for(const x of arr){
+
+//     if()
+
+// }
+
+    
+
+    
+
+}
+
+console.log(maxOccurence([1,2,1,2,3,4,5,6]));
